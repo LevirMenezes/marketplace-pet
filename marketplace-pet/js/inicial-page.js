9702797a65d81779dd746fd2d8ddba3bf5acbea2
Orlando <Orlandoberssane@gmail.com>
2023-10-04 10:39:36 -0400

    mudança de algumas coisinhas

--- marketplace-pet/js/inicial-page.js
+++ marketplace-pet/js/inicial-page.js
@@ -2,9 +2,9 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "marketplace-pet/assets/icons8-menu.svg";
+        document.querySelector('.icon').src = "marketplace-pet/assets/menu_white_36dp.svg";
     } else {
         menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "marketplace-pet/assets/icons8-menu.svg";
+        document.querySelector('.icon').src = "marketplace-pet/assets/close_white_36dp.svg";
     }
 }
